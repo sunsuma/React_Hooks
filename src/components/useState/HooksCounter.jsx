6eed7229e4454fa901442efe508f878d6e3e4723
sunsumabty <114460790+sunsuma@users.noreply.github.com>
2024-05-07 02:21:52 +0530

    switch case

--- src/components/useState/HooksCounter.jsx
+++ src/components/useState/HooksCounter.jsx
@@ -1,28 +1,47 @@
-import { useState } from 'react'
+import { useState } from "react";
 
 function HooksCounter() {
-    // destructure
-    const [count, setCount] = useState(0)
+  // destructure
+  const [count, setCount] = useState(0);
 
-    function handler1(){
-        setCount(count + 1)
+  // function handler1(){
+  //     setCount(count + 1)
+  // }
+  // function handler2(){
+  //     setCount(count>0? count - 1 : 0)
+  // }
+  // function handler3(){
+  //     setCount(0)
+  // }
+
+  function handler(action) {
+    switch (action) {
+      case "increment":
+        setCount(count + 1);
+        break;
+
+      case "decrement":
+        setCount(count > 0 ? count - 1 : 0);
+        break;
+
+      case "reset":
+        setCount(0);
+        break;
+
+      default:
+        break;
     }
-    function handler2(){
-        setCount(count>0? count - 1 : 0)
-    }
-    function handler3(){
-        setCount(0)
-    }
+  }
   return (
     <div>
       <h1>{count}</h1>
       <div>
-        <button onClick={handler1}>Increment</button>
-        <button onClick={handler2}>Decrement</button>
-        <button onClick={handler3}>Reset</button>
+        <button onClick={() => handler("increment")}>Increment</button>
+        <button onClick={() => handler("decrement")}>Decrement</button>
+        <button onClick={() => handler("reset")}>Reset</button>
       </div>
     </div>
-  )
+  );
 }
 
-export default HooksCounter
+export default HooksCounter;
